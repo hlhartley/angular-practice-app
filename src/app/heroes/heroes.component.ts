869885db62e037e51eaac7d161c45cc342cfc9f8
Heather Hartley <hlhartley@gmail.com>
2021-06-13 21:04:02 -0600

    Add routing and restructure files

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -9,20 +9,14 @@ import { MessageService } from '../message.service';
   styleUrls: ['./heroes.component.scss']
 })
 export class HeroesComponent implements OnInit {
-  selectedHero?: Hero;
   heroes: Hero[] = [];
 
   constructor(private heroService: HeroService, private messageService: MessageService) { }
-  ngOnInit() { 
+  ngOnInit() {
     this.getHeroes();
   }
   getHeroes(): void {
     this.heroService.getHeroes()
       .subscribe(heroes => this.heroes = heroes);
-  }
-
-  onSelect(hero: Hero) : void {
-    this.selectedHero = hero;
-    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
 }
